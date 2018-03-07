2dc8f80c1c3afe003018d581d9a1f342625396f0
Nathan Matteson <nwmatteson@gmail.com>
2018-03-07 14:12:25 -0800

    Simple data model for a StackOverflow user.

---
+++ app/src/main/java/io/itsnate/wagchallenge/model/StackOverflowUser.java
@@ -0,0 +1,65 @@
+package io.itsnate.wagchallenge.model;
+
+public class StackOverflowUser {
+    BadgeCounts badgeCounts;
+    String displayName;
+    String gravatarUrl;
+
+    public StackOverflowUser(BadgeCounts badgeCounts, String displayName, String gravatarUrl) {
+        this.badgeCounts = badgeCounts;
+        this.displayName = displayName;
+        this.gravatarUrl = gravatarUrl;
+    }
+
+    public BadgeCounts getBadgeCounts() {
+        return badgeCounts;
+    }
+
+    public void setBadgeCounts(BadgeCounts badgeCounts) {
+        this.badgeCounts = badgeCounts;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static class BadgeCounts {
+        Integer bronze;
+        Integer silver;
+        Integer gold;
+
+        public BadgeCounts(Integer bronze, Integer silver, Integer gold) {
+            this.bronze = bronze;
+            this.silver = silver;
+            this.gold = gold;
+        }
+
+        public Integer getBronze() {
+            return bronze;
+        }
+
+        public void setBronze(Integer bronze) {
+            this.bronze = bronze;
+        }
+
+        public Integer getSilver() {
+            return silver;
+        }
+
+        public void setSilver(Integer silver) {
+            this.silver = silver;
+        }
+
+        public Integer getGold() {
+            return gold;
+        }
+
+        public void setGold(Integer gold) {
+            this.gold = gold;
+        }
+    }
+}
